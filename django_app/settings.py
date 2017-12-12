11a7899b50ac64f4cfac57b2e0086575e6913d92
Boris Bešker <borisbesker@gmail.com>
2017-12-12 17:03:57 +0100

    10

--- django_app/settings.py
+++ django_app/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysite_db',
         'USER': 'boris',
-        'PASSWORD': 'boris',
+        'PASSWORD': '',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
